fa118c78b16b50c8ee1d62b309e434e5852c7115
Ankit Tomar <ankitjdec@gmail.com>
2024-02-08 22:35:53 +0530

    typing function

--- src/aliases.ts
+++ src/aliases.ts
@@ -1,4 +1,4 @@
-interface Contact extends Address {
+interface Contact {
   id: number;
   name: ContactName;
   bithDate?: Date;
@@ -20,9 +20,14 @@ let secondaryContact: Contact = {
   bithDate: new Date("12-12-2012"),
   name: "Ankit",
   id: 2,
-  line1: "w2/1112 Damodarnagar",
-  pincode: 208027,
   status: ContactStatus.Active,
 };
 
 type ContactName = string;
+
+function clone(source: Contact): Contact {
+  return Object.apply({}, source);
+}
+
+const a: Contact = { id: 1, name: "Ankit", status: ContactStatus.Active };
+const c = clone(a);
