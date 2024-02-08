fd463e06fe878caeb75cd5fa72c44409b0073324
Ankit Tomar <ankitjdec@gmail.com>
2024-02-08 20:47:14 +0530

    interfaces finish

--- dist/interfaces/interface.js
+++ dist/interfaces/interface.js
@@ -2,4 +2,6 @@ let primaryContact = {
     bithDate: new Date("12-12-2012"),
     name: "Ankit",
     id: 2,
+    line1: "w2/1112 Damodarnagar",
+    pincode: 208027,
 };
--- src/interfaces/interface.ts
+++ src/interfaces/interface.ts
@@ -1,11 +1,18 @@
-interface Contact {
+interface Contact extends Address {
   id: number;
   name: string;
-  bithDate: Date;
+  bithDate?: Date;
+}
+
+interface Address {
+  line1: string;
+  pincode: number;
 }
 
 let primaryContact: Contact = {
   bithDate: new Date("12-12-2012"),
   name: "Ankit",
   id: 2,
+  line1: "w2/1112 Damodarnagar",
+  pincode: 208027,
 };
