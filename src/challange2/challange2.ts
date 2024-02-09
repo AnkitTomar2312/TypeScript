53a88247b4f1506030bceb5607379e7b3149c59d
Ankit Tomar <ankitjdec@gmail.com>
2024-02-09 18:43:19 +0530

    challange 2 completed one remaining

---
+++ src/challange2/challange2.ts
@@ -0,0 +1,47 @@
+interface Todo {
+  id: number;
+  title: string;
+  completedOn?: Date;
+  status: TodoStatus;
+}
+
+enum TodoStatus {
+  Done = "done",
+  Innprogress = "in-progress",
+  Todo = "todo`",
+}
+
+const todoItems: Todo[] = [
+  {
+    id: 1,
+    title: "Learn HTML",
+    status: "done",
+    completedOn: new Date("2021-09-11"),
+  },
+  { id: 2, title: "Learn TypeScript", status: "in-progress" },
+  { id: 3, title: "Write the best app in the world", status: "todo" },
+];
+
+function addTodoItem(todo) {
+  const id = getNextId(todoItems);
+
+  const newTodo = {
+    id,
+    title: todo,
+    status: "todo",
+  };
+
+  todoItems.push(newTodo);
+
+  return newTodo;
+}
+
+function getNextId(items) {
+  return items.reduce((max, x) => (x.id > max ? x.id : max), 0) + 1;
+}
+
+const newTodo = addTodoItem(
+  "Buy lots of stuff with all the money we make from the app"
+);
+
+console.log(JSON.stringify(newTodo));
